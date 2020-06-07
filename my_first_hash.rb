48c6f08b8019fa13542d46cdaf300fbafc5e475e
Sara Amiry <saraamiry24@gmail.com>
2020-06-07 09:34:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  my_hash = {name => "Sara", gender => "female", hoppy => "coding"}
+  my_hash = {"name" => "Sara", "gender" => "female", "hoppy" => "coding"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
